c5b3ba75d5f5a2b6251d30823b614ba86bc5fb8d
Gsan <754323923@qq.com>
2020-07-19 16:02:19 +0800

    A 项目初始结构构建

---
+++ gulimall-coupon/src/test/java/com/tjj/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjj.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
